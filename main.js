e715ef1336e55ec2a66c974fef12eccb14230275
Mariia Chedryk <mary.chedryk@gmail.com>
2024-11-07 15:07:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const tempF = 80;
+const tempC = (tempF - 32) / 1.8;
+
+console.log(tempF);
+console.log(tempC);
+
+
+const day = 31;
+const hour = day * 24;
+const minutes = day * hour * 60;
+
+console.log(day);
+console.log(hour);
+console.log(minutes);
+
+
+const health = 100;
+const energy = health - 85;
+
+console.log(health);
+console.log(energy);
+
+
+let shop = 500;
+shop -= shop * 0.1;
+
+console.log(shop);
+
+
+const fraction = 19.3045
+const roundNumber = "50";
+const number = 49;
+const num = 784;
+const str = "17px";
+const parsStr = number.parseInt(str);
+const parsNum = num.toString();
+ 
+console.log(Math.floor(fraction));
+console.log(number.parseInt(roundNumber ));
+console.log(Math.sqrt(number));
+console.log(parsStr);
+console.log(parsNum);
